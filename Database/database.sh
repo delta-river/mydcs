ff086744b562cf206ae38097f3ffdaf309a878e7
delta-river <taigarana@LAPTOP-Q2DC233A.localdomain>
2020-05-09 22:39:15 +0900

    enabled to add tag information

--- Database/database.sh
+++ Database/database.sh
@@ -8,6 +8,8 @@ SOURCE[1]=${DATABASE}-fields.txt
 SOURCE[2]=${DATABASE}-schema.csv
 SOURCE[3]=${DATABASE}.json
 MAIN_SOURCE=${SOURCE[0]}
+#tag file
+TAG=../${DATABASE}.tag
 
 function expect_pass(){
 	expect -c "
@@ -16,6 +18,32 @@ function expect_pass(){
 	send \"$2\n\"
 	interact
 	"
+}
+
+tag=()
+function tag_insert(){
+	#$1 out file
+	#$2 table name
+	#$3 number of columns
+	j=0
+	for ((k=0; k<${#tag[@]}-1; k=$k+2))
+	do
+		if [ "${tag[$k]}" = "$2:" ]; then
+			#count number of tags
+			counttag=`echo -e "${tag[$k+1]}" | tr '\t' '\n' | wc -l`
+			#check if the number of columns of the table and the number of the tags are the same
+			if [ ${counttag} = $3 ]; then
+				echo "${tag[$k+1]}" >> $1
+				j=${j}+1
+				break
+			else
+				echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!number of tags missmatched for table $2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+			fi
+		fi
+	done
+	if [ ${j} = 0 ]; then
+		echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!no tag is set for table $2!!!!!!!!!!!!!!!!!!!!!!!!"
+	fi
 }
 
 #reset database
@@ -55,12 +83,26 @@ done < table_name_tmp
 
 rm table_name_tmp
 
+i=0
+while read line
+do 
+	tag[i]=${line}
+	i=${i}+1
+done < ${TAG}
+
 #add tables to database
 #execpt the database name on the top
 for ((i=1; i<${#tables[@]}; i++))
 do
-	echo "select * from ${tables[$i]};" > tmp.sql
+	#count the number of columns
+	echo "select count(*) from information_schema.columns where table_name ='${tables[$i]}';" > tmp.sql
+	expect_pass "mysql -N -u root -p ${DATABASE} < tmp.sql > tmp" ${PASSWORD}
+	count=`cat tmp`
+
 	echo "${tables[$i]}:" >> ../database.txt
+	tag_insert "../database.txt" ${tables[$i]} ${count}
+
+	echo "select * from ${tables[$i]};" > tmp.sql
 	expect_pass "mysql -u root -p ${DATABASE} < tmp.sql >> ../database.txt" ${PASSWORD}
 done
 
@@ -68,4 +110,5 @@ done
 echo "drop database ${DATABASE};" > tmp.sql
 expect_pass "mysql -u root -p ${DATABASE} < tmp.sql" ${PASSWORD}
 
+rm tmp
 rm tmp.sql
---
+++ Database/geography.tag
@@ -0,0 +1,14 @@
+border_info:
+state	state
+city:
+city	number	country	state
+highlow:
+state	number	place	place	number
+lake:
+lake	number	country	state
+mountain:
+mountain	number	country	state
+river:
+river	number	country	state
+state:
+state	number	number	country	city	number
